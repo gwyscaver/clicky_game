5caf1509ad950c74ac84a0b85ef38f601c530d11
George Wyscaver <gwyscaver3@gmail.com>
2019-10-25 23:09:03 -0700

    Updates

---
+++ precache-manifest.7dac7f045acd6961473eaeae72bb64fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b19423df3be5b72cf256ada905be9b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ed54a28bc20fa732f9e6",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "99fcc92ca85cebd3d3e5",
+    "url": "/my-app/static/js/2.1da6d8e2.chunk.js"
+  },
+  {
+    "revision": "ed54a28bc20fa732f9e6",
+    "url": "/my-app/static/js/main.5f3f3319.chunk.js"
+  },
+  {
+    "revision": "d168a156937ef92bffe9",
+    "url": "/my-app/static/js/runtime-main.beff2451.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
